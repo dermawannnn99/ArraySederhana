c3d2ce40cb1035d19be13933e03fb8396c81ea7b
DERMANRAY <rizkydermawan12345@gmail.com>
2024-12-15 13:40:44 +0700

    Add files via upload

---
+++ Array Sederhana.js
@@ -0,0 +1,9 @@
+let NamaMakanan = ['ayam', 'nasi goreng', 'telur dadar', 'bakso', 'lalapan']
+
+//memanggil nama makanan dengan indeks ke-0
+console.log(
+    NamaMakanan[0] //output : ayam
+)
+console.log(
+    NamaMakanan[4]
+)
